574f786d2eaa1a0307f7e51e71ddd3112c70cfcc
tap1ra <yasuaki.tahira@gmail.com>
2025-01-01 15:18:43 +0900

    construct_tree init

---
+++ saba_core/src/renderer/html/parser.rs
@@ -0,0 +1,58 @@
+use crate::renderer::dom::node::Node;
+use crate::renderer::dom::node::Window;
+use crate::renderer::html::token::HtmlTokenizer;
+use alloc::rc::Rc;
+use alloc::vec::Vec;
+use core::cell::RefCell;
+
+#[derive(Debug, Clone)]
+pub struct HtmlParser {
+    window: Rc<RefCell<Window>>,
+    mode: InsertionMode,
+    original_insertion_mode: InsertionMode,
+    stack_of_open_elements: Vec<Rc<RefCell<Node>>>,
+    t: HtmlTokenizer,
+}
+
+impl HtmlParser {
+    pub fn new(t: HtmlTokenizer) -> Self {
+        Self {
+            window: Rc::new(RefCell::new(Window::new())),
+            mode: InsertionMode::Initial,
+            original_insertion_mode: InsertionMode::Initial,
+            stack_of_open_elements: Vec::new(),
+            t,
+        }
+    }
+
+    pub fn construct_tree(&mut self) -> Rc<RefCell<Window>> {
+        let mut token = self.t.next();
+
+        while token.is_some() {
+            match self.mode {
+                InsertionMode::Initial => {}
+                InsertionMode::BeforeHtml => {}
+                InsertionMode::BeforeHead => {}
+                InsertionMode::InHead => {}
+                InsertionMode::AfterHead => {}
+                InsertionMode::InBody => {}
+                InsertionMode::Text => {}
+                InsertionMode::AfterBody => {}
+                InsertionMode::AfterAfterBody => {}
+            }
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum InsertionMode {
+    Initial,
+    BeforeHtml,
+    BeforeHead,
+    InHead,
+    AfterHead,
+    InBody,
+    Text,
+    AfterBody,
+    AfterAfterBody,
+}
